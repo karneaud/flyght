8231a84fe500dffbb5853a4a01f2efa7339f8a73
Kendall Arneaud <kendall.arneaud@gmail.com>
2022-12-30 00:05:36 +0000

    refactor tests using tools

--- test.js
+++ test.js
@@ -1,5 +1,22 @@
-var flyght = require("./flyght");
+import { describe, it } from 'mocha'
+import { expect, assert, should } from 'chai'
+import sinon from 'sinon'
+import Flyght from './src/index.js'
+
+should()
+
+describe('Flyght class tests',() => {
+    it('should exists',() => {
+        Flyght.should.exist
+        Flyght.should.be.a('function')
+    })
+
+    it('should instantiate class',() =>{
+        sinon.replace(Flyght.prototype,'init',sinon.fake())
+        sinon.replace(Flyght.prototype,'register',sinon.fake())
+        expect(new Flyght({})).instanceOf(Flyght)
+    })
+})
 
 
 
-console.log(flyght);
